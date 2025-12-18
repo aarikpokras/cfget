ff857758be569a3c55fa8aec147e5ec14b8c3629
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2025-12-18 00:09:46 -0500

    Improve error messages and output formatting

--- src/main.go
+++ src/main.go
@@ -21,7 +21,7 @@ func main() {
     }
     defer rsp.Body.Close()
     if rsp.StatusCode == http.StatusNotFound {
-      fmt.Println("No snippet found for " + os.Args[2] + " :(")
+      fmt.Println("No snippet found for " + os.Args[2] + " :(\nMake sure you're being as specific as possible; for example, instead of using hyprland/blur, try hyprland/decoration/blur.")
       os.Exit(1)
     }
     if rsp.StatusCode != http.StatusOK {
@@ -46,17 +46,18 @@ func main() {
       }
       defer rsp.Body.Close()
       if rsp.StatusCode == http.StatusNotFound {
-        fmt.Println("-------------------\nDone with snippet list. " + strconv.Itoa(i) + " snippets found.")
-        os.Exit(1)
+        fmt.Println("--------------------\nDone with snippet list. " + strconv.Itoa(i) + " snippets found.")
+        os.Exit(0)
       }
       if rsp.StatusCode != http.StatusOK {
         fmt.Println("HTTP error " + rsp.Status)
       }
       bdy, err := io.ReadAll(rsp.Body)
       fmt.Println(string(bdy))
+      fmt.Println("********************")
     }
   } else {
-    fmt.Printf("Invalid flag! Ensure counts and flags are separated and that you're using correct flags!\n\e[0;34mFrom exception in main -- unrecognized argument\e[0m\n")
+    fmt.Printf("Invalid flag! Ensure counts and flags are separated and that you're using correct flags!\n\033[0;34mFrom exception in main -- unrecognized argument\033[0m\n")
     os.Exit(4)
   }
 }
