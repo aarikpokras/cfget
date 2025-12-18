d4d782f4f0ec30af2a8f75c34233d61adbd1c1ff
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2025-12-18 12:26:04 -0500

    Add `CFGET_HIDE_ERRORS` env var

--- main.go
+++ main.go
@@ -9,9 +9,15 @@ import (
   "strconv"
 )
 
+func hideerrors() bool {
+  return os.Getenv("CFGET_HIDE_ERRORS") == "1"
+}
+
 func main() {
   if len(os.Args) < 3 {
-    fmt.Println("Usage: " + os.Args[0] + " [options] [tags]\nOPTIONS:\n-s			Get first snippet.\n--ctmax [count]		Get [count] snippets.\n--gos [num]		get the [num]th snippet.")
+    if !hideerrors() {
+      fmt.Println("Usage: " + os.Args[0] + " [options] [tags]\nOPTIONS:\n-s			Get first snippet.\n--ctmax [count]		Get [count] snippets.\n--gos [count]		get the [count]th snippet.")
+    }
     os.Exit(2)
   }
   if (os.Args[1] == "-s") {
@@ -21,22 +27,30 @@ func main() {
     }
     defer rsp.Body.Close()
     if rsp.StatusCode == http.StatusNotFound {
-      fmt.Println("No snippet found for " + os.Args[2] + " :(\nMake sure you're being as specific as possible; for example, instead of using hyprland/blur, try hyprland/decoration/blur.")
+      if !hideerrors() {
+        fmt.Println("No snippet found for " + os.Args[2] + " :(\nMake sure you're being as specific as possible; for example, instead of using hyprland/blur, try hyprland/decoration/blur.")
+      }
       os.Exit(1)
     }
     if rsp.StatusCode != http.StatusOK {
-      fmt.Println("HTTP error " + rsp.Status)
+      if !hideerrors() {
+        fmt.Println("HTTP error " + rsp.Status)
+      }
     }
     bdy, err := io.ReadAll(rsp.Body)
     fmt.Println(string(bdy))
   } else if (os.Args[1] == "--ctmax") {
     if len(os.Args) < 4 {
-      fmt.Println("--ctmax requires a count and tags.")
+      if !hideerrors() {
+        fmt.Println("--ctmax requires a count and tags.")
+      }
       os.Exit(4)
     }
     ctmax, err := strconv.Atoi(os.Args[2])
     if err != nil {
-      fmt.Printf("Error in argument 2: %v\n", err)
+      if !hideerrors() {
+        fmt.Printf("Error in argument 2: %v\n", err)
+      }
       os.Exit(8)
     }
     for i := 0; i < ctmax; i++ {
@@ -50,7 +64,9 @@ func main() {
         os.Exit(0)
       }
       if rsp.StatusCode != http.StatusOK {
-        fmt.Println("HTTP error " + rsp.Status)
+        if !hideerrors() {
+          fmt.Println("HTTP error " + rsp.Status)
+        }
       }
       bdy, err := io.ReadAll(rsp.Body)
       fmt.Println(string(bdy))
@@ -58,12 +74,16 @@ func main() {
     }
   } else if (os.Args[1] == "--gos") {
     if len(os.Args) < 4 {
-      fmt.Println("--gos requires a count and tags.")
+      if !hideerrors() {
+        fmt.Println("--gos requires a count and tags.")
+      }
       os.Exit(4)
     }
     _, err := strconv.Atoi(os.Args[2])
     if err != nil {
-      fmt.Println("Argument 2 is not a number.")
+      if !hideerrors() {
+        fmt.Println("Argument 2 is not a number.")
+      }
       os.Exit(8)
     }
     rsp, err := http.Get("https://raw.githubusercontent.com/aarikpokras/cfget/refs/heads/master/snippets/" + os.Args[3] + os.Args[2])
@@ -72,17 +92,23 @@ func main() {
     }
     defer rsp.Body.Close()
     if rsp.StatusCode == http.StatusNotFound {
-      fmt.Println("Could not find that snippet.")
+      if !hideerrors() {
+        fmt.Println("Could not find that snippet.")
+      }
       os.Exit(1)
     }
     if rsp.StatusCode != http.StatusOK {
-      fmt.Println("HTTP error " + rsp.Status)
+      if !hideerrors() {
+        fmt.Println("HTTP error " + rsp.Status)
+      }
       os.Exit(1)
     }
     bdy, err := io.ReadAll(rsp.Body)
     fmt.Println(string(bdy))
   } else {
-    fmt.Printf("Invalid flag! Ensure counts and flags are separated and that you're using correct flags!\n\033[0;34mFrom exception in main -- unrecognized argument\033[0m\n")
+    if !hideerrors() {
+      fmt.Printf("Invalid flag! Ensure counts and flags are separated and that you're using correct flags!\n\033[0;34mFrom exception in main -- unrecognized argument\033[0m\n")
+    }
     os.Exit(4)
   }
 }
