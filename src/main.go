de395ee9e6288b42e75519cb0bd306e42ff6e2cf
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2025-12-18 00:05:29 -0500

    More detailed error messages and new `--ctmax` for desired amount of snippets

--- src/main.go
+++ src/main.go
@@ -6,15 +6,16 @@ import (
   "io"
   "os"
   "log"
+  "strconv"
 )
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Println("Usage: " + os.Args[0] + " [tags]")
+  if len(os.Args) < 3 {
+    fmt.Println("Usage: " + os.Args[0] + " -s [tags]")
     os.Exit(2)
   }
   if (os.Args[1] == "-s") {
-    rsp, err := http.Get("https://raw.githubusercontent.com/aarikpokras/cfget/refs/heads/master/snippets/" + os.Args[2])
+    rsp, err := http.Get("https://raw.githubusercontent.com/aarikpokras/cfget/refs/heads/master/snippets/" + os.Args[2] + "0")
     if err != nil {
       log.Fatal(err)
     }
@@ -28,8 +29,34 @@ func main() {
     }
     bdy, err := io.ReadAll(rsp.Body)
     fmt.Println(string(bdy))
+  } else if (os.Args[1] == "--ctmax") {
+    if len(os.Args) < 4 {
+      fmt.Println("--ctmax requires a count and tags.")
+      os.Exit(4)
+    }
+    ctmax, err := strconv.Atoi(os.Args[2])
+    if err != nil {
+      fmt.Printf("Error in argument 2: %v\n", err)
+      os.Exit(8)
+    }
+    for i := 0; i < ctmax; i++ {
+      rsp, err := http.Get("https://raw.githubusercontent.com/aarikpokras/cfget/refs/heads/master/snippets/" + os.Args[3] + strconv.Itoa(i))
+      if err != nil {
+        log.Fatal(err)
+      }
+      defer rsp.Body.Close()
+      if rsp.StatusCode == http.StatusNotFound {
+        fmt.Println("-------------------\nDone with snippet list. " + strconv.Itoa(i) + " snippets found.")
+        os.Exit(1)
+      }
+      if rsp.StatusCode != http.StatusOK {
+        fmt.Println("HTTP error " + rsp.Status)
+      }
+      bdy, err := io.ReadAll(rsp.Body)
+      fmt.Println(string(bdy))
+    }
   } else {
-    fmt.Println("Invalid flag.")
+    fmt.Printf("Invalid flag! Ensure counts and flags are separated and that you're using correct flags!\n\e[0;34mFrom exception in main -- unrecognized argument\e[0m\n")
     os.Exit(4)
   }
 }
